bef63ed443dde8e07fab0984b0398b41c357f239
Kristjan Erik Pedak <kristjan.pedak@gmail.com>
2018-03-02 18:21:40 +0200

    Lombak fail, revert

--- src/main/java/app/ride/Ride.java
+++ src/main/java/app/ride/Ride.java
@@ -4,8 +4,7 @@ package app.ride;
 import java.util.Date;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.Getter;
-import lombok.Setter;
+
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,8 +12,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@Getter
-@Setter
 public class Ride {
 	@Id
 	@GeneratedValue
@@ -24,4 +21,27 @@ public class Ride {
 	Date starttime;
 	int freeseats;
 	double ridefee;
+	public Ride (String start, String end, Date time, int seats, double fee) {
+		this.startpoint = start;
+		this.endpoint = end;
+		this.starttime = time;
+		this.freeseats = seats;
+		this.ridefee = fee;
+
+	}
+	public String getstartpoint() {
+		return startpoint;
+	}
+	public String getendpoint() {
+		return endpoint;
+	}
+	public Date getstarttime() {
+		return starttime;
+	}
+	public int getfreeseats() {
+		return freeseats;
+	}
+	public double getridefee() {
+		return ridefee;
+	}
 }

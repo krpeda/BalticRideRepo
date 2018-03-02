2046317fd40a00e068e2fcbf92ad4ff59425e5d5
Kristjan Erik Pedak <kristjan.pedak@gmail.com>
2018-03-02 17:16:53 +0200

    Ride @Getter @Setter

--- src/main/java/app/ride/Ride.java
+++ src/main/java/app/ride/Ride.java
@@ -4,14 +4,18 @@ package app.ride;
 import java.util.Date;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Ride {
-
 	@Id
 	@GeneratedValue
 	long id;
@@ -20,28 +24,4 @@ public class Ride {
 	Date starttime;
 	int freeseats;
 	double ridefee;
-	
-	public Ride (String start, String end, Date time, int seats, double fee) {
-		this.startpoint = start;
-		this.endpoint = end;
-		this.starttime = time;
-		this.freeseats = seats;
-		this.ridefee = fee;
-
-	}
-	public String getStartpoint() {
-		return startpoint;
-	}
-	public String geEndpoint() {
-		return endpoint;
-	}
-	public Date getStarttime() {
-		return starttime;
-	}
-	public int getFreeseats() {
-		return freeseats;
-	}
-	public double getRidefee() {
-		return ridefee;
-	}
 }

2c9c15f18fd0fa095219bd96993096c02e3b5398
Valeri <valerikrusinin@gmail.com>
2018-03-07 08:37:26 +0200

    navbar fix

--- BalticRide/src/app.js
+++ BalticRide/src/app.js
@@ -27,8 +27,7 @@ export class App {
       {route: 'destination', name: 'dest', moduleId: 'dest', title: 'Add Ride'},
       {route: 'rides', name: 'rides', moduleId: 'rides', title: 'Find Ride'},
       {route: 'about', name:'about', moduleId:'about', title:'About Us'},
-      {route: 'blog', name:'blog', moduleId: 'blog', title:'Our Blog' },
-      {route:'allrides', name:'allrides', moduleId:'all', title:'All Rides'}
+      {route:'allrides', name:'all', moduleId:'all', title:'All Rides'}
 
 
     ]);

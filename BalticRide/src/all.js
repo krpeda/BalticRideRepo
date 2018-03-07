c928e8839b6da0e11eff60249c1a5e4b38ad6923
Valeri <valerikrusinin@gmail.com>
2018-03-07 08:33:38 +0200

    find all front

--- BalticRide/src/all.js
+++ BalticRide/src/all.js
@@ -1,5 +1,35 @@
 import {HttpClient, json} from 'aurelia-fetch-client';
 
 export class All {
+
   
+
+  rideList = [
+    {"startpoint": "Tallinn",
+    "endpoint": "riga",
+    "starttime" :23,
+    "freeseats": 2,
+    "ridefee": 50
+  },
+  {"startpoint": "Tallinn",
+    "endpoint": "riga",
+    "starttime" :23,
+    "freeseats": 2,
+    "ridefee": 50
+  },
+  {"startpoint": "Tallinn",
+    "endpoint": "riga",
+    "starttime" :23,
+    "freeseats": 2,
+    "ridefee": 50
+  }
+  ]
+  
+  activate() {
+
+    let client = new HttpClient();
+    client.fetch('http://localhost:8080/rides')
+      .then(response => response.json())
+      .then(rides => this.rideList = rides);
+  }
 }

dacf3267fddddec8c8a998d5a56993ea97fc3dbf
Valeri <valerikrusinin@gmail.com>
2018-05-14 16:34:49 +0300

    join

--- BalticRide/src/allrides.js
+++ BalticRide/src/allrides.js
@@ -15,25 +15,15 @@ export class All {
         for (var i = 0; i < rides.length; i++) {
             rides[i].startTime = moment(rides[i].startTime).format("DD-MM-YYYY HH:mm");
         }
-        console.log(rides);
       });
   }
-  onClickJoin() {
+  onClickJoin(rides) {
+    let index = this.rideList.indexOf(rides);
+    let rideId = this.rideList[index].id;
     const userId = firebase.auth().currentUser.uid;
-    let rideCar = this.rides.rideCar;
+    let car = this.rideList[index].rideCar.userId;
+    console.log(car);
     console.log(userId);
-    console.log(rideCar);
-    let client = new HttpClient();
-    client.fetch('http://localhost:8080/rides')
-      .then(response => response.json())
-      .then(rides => {
-        /*this.rideList.startTime = moment.unix(this.rideList.startTime).format("yyyy-MM-dd HH:mm");*/
-        this.rideList = rides; 
-        for (var i = 0; i < rides.length; i++) {
-            rides[i].startTime = moment(rides[i].startTime).format("DD-MM-YYYY HH:mm");
-        }
-        console.log(rides);
-      });
-
+    console.log(rideId);
   }
 }
